48f4a3137e6b791250f9208b50db95470c59028b
Sergio395 <tolser2019@gmail.com>
2023-04-15 17:38:15 -0300

    Seguimiento-Area-fecha-Novedades

--- gestion_reclamos/reclamos/views.py
+++ gestion_reclamos/reclamos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+import datetime
 # Create your views here.
 
 def index(request):
@@ -23,4 +23,8 @@ def carga_certificado(request):
     return render(request, 'reclamos/carga_certificaciones.html', {})
 
 
-
+def seguimiento(request):
+    fechaActual= datetime.datetime.now()
+    nombre= "Area geográfica norte"
+    lista = ['Arbol caido = 1', 'Arbol enfermo=2', 'Arbol poda=5', 'Arbol no identificado=2']
+    return render(request,'reclamos/seguimiento.html',{'fechaActual': fechaActual, 'nombre': nombre, 'lista':lista})
