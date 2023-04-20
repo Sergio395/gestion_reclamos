ae3fd6179ff95ef76b2eb2954e3ca4fc7cfadc32
Cristian Lahoz <lahozcristian@gmail.com>
2023-04-19 22:42:34 -0300

    muevo templates y statics a BASE_DIR

--- gestion_reclamos/gestion_reclamos/urls.py
+++ gestion_reclamos/gestion_reclamos/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-#from reclamos import views as views_reclamos 
 
 
 urlpatterns = [
--- gestion_reclamos/reclamos/views.py
+++ gestion_reclamos/reclamos/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 import datetime
-# Create your views here.
+from django.shortcuts import render
 
+
+# Create your views here.
 def index(request):
     return render(request, 'reclamos/index.html', {})
 
@@ -23,15 +22,26 @@ def carga_inspeccion(request):
 def carga_certificado(request):
     return render(request, 'reclamos/carga_certificaciones.html', {})
 
-lista=['Carlos','Garcia', 30333256, 15995687,42974589, 'pepe@nocorreo.com']
+
+lista = ['Carlos', 'Garcia', 30333256, 15995687, 42974589, 'pepe@nocorreo.com']
 lista2 = ['Arbol caido = 1', 'Arbol enfermo = 2', 'Arbol poda = 5', 'Arbol no identificado = 2']
 # lista2 = []
 
+
 def seguimiento(request):
-    fechaActual= datetime.datetime.now()
-    nombre= lista[0] + " " + lista[1]
-    return render(request, 'reclamos/seguimiento.html', {'fechaActual': fechaActual, 'nombre': nombre, 'lista2': lista2, 'lista':lista})
+    fecha_actual = datetime.datetime.now()
+    nombre = lista[0] + " " + lista[1]
+    return render(request, 'reclamos/seguimiento.html', {
+        'fechaActual': fecha_actual,
+        'nombre': nombre,
+        'lista2': lista2,
+        'lista': lista
+    })
 
 
 def seguimiento_reclamo(request, nro_reclamo):
-    return render(request, 'reclamos/ver_reclamo.html', {'lista2': lista2[nro_reclamo-1], 'nro': nro_reclamo,'lista':lista})
+    return render(request, 'reclamos/ver_reclamo.html', {
+        'lista2': lista2[nro_reclamo-1],
+        'nro': nro_reclamo,
+        'lista': lista
+    })
