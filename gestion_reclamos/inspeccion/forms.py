4825c13b9ae0d2e8f195bce585693611beb47db6
Emiliano Salvadeo <salvadeo@agro.uba.ar>
2023-04-23 12:10:13 -0300

    formulario carga inspecciones

--- gestion_reclamos/inspeccion/forms.py
+++ gestion_reclamos/inspeccion/forms.py
@@ -8,5 +8,36 @@ class ContactoForm(forms.Form):
     nota = forms.CharField(label='Observaciones',widget=forms.Textarea(attrs={'row':1,'cols':23}),required=False)
     
    
+class NuevaInspeccion(forms.Form):
+    CHOICES = [("1", "First"), ("2", "Second")]
+    numero_reclamo = forms.CharField(label='Ingrese numero de reclamos') 
+    reclamo_valido = forms.CharField(label='Reclamo valido?') 
+    fecha_inspeccion = forms.CharField(label='Ingrese fecha de inspeccion') 
+    trabajo = forms.CharField(label='Selecciones trbajo a realizar') 
+    especie = forms.CharField(label='Ingrese especie') 
+    altura = forms.CharField(label='Ingrese altura (m)') 
+    dap = forms.CharField(label='Ingrese DAP (cm)') 
+    cableado = forms.CharField(label='Cableado cercano') 
+    construccion = forms.CharField(label='Construcciones cercanas') 
+    observaciones = forms.CharField(label='Observaciones') 
+    urgencia = forms.CharField(label='Selecciones urgencia') 
+    justificacion = forms.CharField(label='Justifique urgencia') 
+    inspector = forms.CharField(label='Inspector') 
     
-  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+                
+    
+    
+    
+    
+    
