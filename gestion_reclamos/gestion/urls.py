423440741d6f2c6891668a967a94d414ed2cae67
Ezequiel Mazzini <ezequiel.mazzini@bue.edu.ar>
2023-04-29 16:33:47 -0300

    algunas mejoras y limpieza en gestion

--- gestion_reclamos/gestion/urls.py
+++ gestion_reclamos/gestion/urls.py
@@ -4,6 +4,6 @@ from gestion import views
 
 
 urlpatterns = [
-    path('', views.gestion_ver_reclamos, name='gestion'),
+    path('', views.gestion_inicio, name='gestion'),
     path('/editar', views.gestion_editar_reclamo, name='gestion_editar_reclamo'),
 ]
