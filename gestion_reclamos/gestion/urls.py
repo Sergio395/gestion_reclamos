6c613252e5417567ef0288f031eca1e25e20bbf6
Ezequiel Mazzini <ezequiel.mazzini@bue.edu.ar>
2023-04-29 17:32:47 -0300

    se agrego edicion de reclamos (falta completar)

--- gestion_reclamos/gestion/urls.py
+++ gestion_reclamos/gestion/urls.py
@@ -5,5 +5,6 @@ from gestion import views
 
 urlpatterns = [
     path('', views.gestion_inicio, name='gestion'),
-    path('/editar', views.gestion_editar_reclamo, name='gestion_editar_reclamo'),
+    path('editar', views.gestion_editar_reclamo, name='gestion_editar_reclamo'),
+    path('editar/<int:nro_reclamo>/', views.gestion_editar_reclamo, name='gestion_editar_reclamo')
 ]
--- gestion_reclamos/gestion/views.py
+++ gestion_reclamos/gestion/views.py
@@ -248,21 +248,26 @@ def gestion_inicio (request):
 
     return render(request, 'gestion/gestion_inicio.html', context)
 
-def gestion_editar_reclamo (request):
-    mensaje = None
+# def gestion_editar_reclamo (request):
+#     mensaje = None
 
-    if request.method == 'POST':
-        gestion_form = GestionForm(request.POST)
-        mensaje = 'Hemos recibido tus datos'
-    # acción para tomar los datos del formulario
-    elif request.method == 'GET':
-        gestion_form = GestionForm()
-    else:
-        return HttpResponseNotAllowed(f"Método {request.method} no soportado")
+#     if request.method == 'POST':
+#         gestion_form = GestionForm(request.POST)
+#         mensaje = 'Hemos recibido tus datos'
+#     # acción para tomar los datos del formulario
+#     elif request.method == 'GET':
+#         gestion_form = GestionForm()
+#     else:
+#         return HttpResponseNotAllowed(f"Método {request.method} no soportado")
 
-    context = {
-        'mensaje': mensaje,
-        'gestion_form': gestion_form
-    }
+#     context = {
+#         'mensaje': mensaje,
+#         'gestion_form': gestion_form
+#     }
 
-    return render(request, 'gestion/gestion_index.html', context)
+#     return render(request, 'gestion/gestion_index.html', context)
+
+def gestion_editar_reclamo(request, nro_reclamo):
+    return render(request, 'gestion/gestion_ver_reclamo.html', {
+        'reclamo': lista_reclamos[nro_reclamo-1],
+    })
