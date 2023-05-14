c8bd526d89923584fe169c48c3f943c1fb085f92
Sergio Tolaba <tolseryi@gmail.com>
2023-05-14 12:31:28 -0300

    inicial model

--- apps/inspeccion/models.py
+++ apps/inspeccion/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Inspeccion(models.Model):
+    
+    
+    reclamo = models.CharField(max_length=100, verbose_name="Reclamo")
+    inspector = models.CharField(max_length=25, verbose_name="Apellido")
+    nota = models.CharField(max_length=300, verbose_name="nota")
+    foto = models.ImageField(upload_to='img_reclamos', null=True, blank=True, verbose_name="Fotos")
+    
+    def __str__(self):
+        return f'Reclamo {self.numero}'
