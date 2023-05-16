d73cf471cd1f86cde3eab2b2405cbbf5f6186ad8
Emiliano Salvadeo <salvadeo@agro.uba.ar>
2023-05-16 19:20:08 -0300

    models varios

--- apps/administracion/models.py
+++ apps/administracion/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
 class Usuario(models.Model):
+    
     """
     Modelo base para usuarios.
+    
     """
 
     fecha_creacion = models.DateField(auto_now_add=True, verbose_name="Fecha de creación")
@@ -21,37 +24,17 @@ class Usuario(models.Model):
         abstract = True
         
         
-
-class Empresa(models.Model):
-    """
-    Modelo base para empresas.
-    """
-
-
-    fecha_alta = models.DateField(auto_now_add=True, verbose_name="Fecha_alta")
-    nombre = models.CharField(max_length=30, verbose_name="Nombre")
-    razon_social = models.CharField(max_length=30, verbose_name="Razon_social")
-    num_proveedor = models.IntegerField(max_length=15, verbose_name="Num_proveedor")
-    cuit = models.IntegerField(verbose_name="Cuit")
-    correo = models.EmailField(max_length=20, verbose_name="Correo_Electronico")
-    telefono = models.CharField(max_length=20, verbose_name="Telefono")
-
-    def __str__(self):
-        return f"{self.razon_social} => {self.num_proveedor}"
-
-    class Meta:
-        abstract = False
         
         
 class OrdenCompra(models.Model):
     """
     Modelo base para ordenes de compras.
+       
     """
 
-
     fecha_emision = models.DateField(auto_now_add=True, verbose_name="Fecha_emision")
-    empresa = models.CharField(max_length=30, verbose_name="Empresa")
-    numero = models.IntegerField(max_length=20, verbose_name="Numero")
+    proveedor = models.CharField(max_length=30, verbose_name="Proveedor")
+    numero = models.IntegerField(verbose_name="Numero")
     cantidad = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Cantidad")
     descripcion = models.TextField(verbose_name="Descripcion")
     precio_unitario = models.DecimalField(max_digits=10, decimal_places=2,verbose_name="Precio_unitario")
@@ -63,10 +46,62 @@ class OrdenCompra(models.Model):
     
 
     def __str__(self):
-        return f"{self.empresa} => {self.numero}"
+        return f"{self.proveedor} / {self.numero}"
 
     class Meta:
         abstract = False
+
+
+class Empresa(models.Model):
+    
+    """
+    Modelo base para empresas.
+    """
+
+
+    fecha_alta = models.DateField(auto_now_add=True, verbose_name="Fecha_alta")
+    nombre = models.CharField(max_length=30, verbose_name="Nombre")
+    razon_social = models.CharField(max_length=30, verbose_name="Razon_social")
+    num_proveedor = models.IntegerField(verbose_name="Num_proveedor")
+    cuit = models.IntegerField(verbose_name="Cuit")
+    correo = models.EmailField(max_length=20, verbose_name="Correo_Electronico")
+    telefono = models.CharField(max_length=20, verbose_name="Telefono")
+    orden_compra = models.ForeignKey(OrdenCompra, on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return f"{self.razon_social} - {self.orden_compra}"
+
+    class Meta:
+        abstract = False
+        
+        
+
+        
+class Cuadrante(models.Model):
+    
+    """
+    Modelo base para cuadrantes.
+    """
+
+
+    nombre = models.CharField(max_length=30, verbose_name="Nombre")
+    punto1_lat  = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Punto_1_latitud")
+    punto1_long = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Punto_1_longitud")
+    punto2_lat  = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Punto_2_latitud")
+    punto2_long = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Punto_2_longitud")
+    punto3_lat  = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Punto_3_latitud")
+    punto3_long = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Punto_3_longitud")
+    punto4_lat  = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Punto_4_latitud")
+    punto4_long = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Punto_4_longitud")
+    
+    
+    def __str__(self):
+        
+        return f"{self.nombre}"
+
+    class Meta:
+        abstract = False
+
 
 
 
