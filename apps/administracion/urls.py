3acec1d921a310987627f78d65dd2b1b3d68f185
Emiliano Salvadeo <salvadeo@agro.uba.ar>
2023-06-07 23:18:59 -0300

    merge con temp_emi

--- apps/administracion/urls.py
+++ apps/administracion/urls.py
@@ -7,14 +7,12 @@ urlpatterns = [
     path('user/', views.UserListView.as_view(), name='usuario'),
     path('nuevo_usuario/', views.UserCreateView.as_view(), name='nuevo_usuario'), 
     path('user/<int:pk>/', views.UserUpdateView.as_view(), name='editar_usuario'),
-    path('user/delete/<int:pk>/', views.delete_usuario, name='eliminar_usuario'),
-    
+    path('user/delete/<int:pk>/', views.delete_usuario, name='eliminar_usuario'),    
     
     path('empresa/', views.ListarEmpresas.as_view(), name='empresa'),
     path('nueva_empresa/', views.nueva_empresa, name='nueva_empresa'),
     path('empresa/<int:id_empresa>/', views.editar_empresa, name='editar_empresa'),
-    path('empresa/delete/<int:id_empresa>/', views.delete_empresa, name='eliminar_empresa'),
-    
+    path('empresa/delete/<int:id_empresa>/', views.delete_empresa, name='eliminar_empresa'),    
     
     path('orden_compra/', views.OrdencompraListView.as_view(), name='orden_compra'),
     path('nueva_oc/', views.OrdencompraCreateView.as_view(), name='nueva_oc'),
