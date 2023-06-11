26ff1410b2b8531b7545dad6eed7359f6da95505
Sergio395 <tolser2019@gmail.com>
2023-06-10 21:13:18 -0300

    adm inspeccion columnas

--- apps/inspeccion/admin.py
+++ apps/inspeccion/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+
 @admin.register(Inspeccion)
-
 class InspeccionAdmin(admin.ModelAdmin):
-    list_display = ('reclamo','inspector','lugar','fecha_inspeccion','trabajo')
+    list_display = ('reclamo','inspector',
+                    'lugar','fecha_inspeccion','trabajo')
 
 
-#admin.site.register(Inspeccion,InspeccionAdmin)
-
-
-admin.site.register(Arbol)
+@admin.register(Arbol)
+class ArbolAdmin(admin.ModelAdmin):
+    list_display = ('inspeccion','urgencia')
+    
