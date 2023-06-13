9d4483463509c47bfd3a46f8bc4d5a1389e6d788
Ezequiel Mazzini <ezequiel.mazzini@bue.edu.ar>
2023-06-13 00:20:52 -0300

    resta busqueda en gestionlista

--- apps/gestion/urls.py
+++ apps/gestion/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.gestion_index, name='gestion'),
-    path('', views.gestion_index, name='gestion_index'),
+    # path('', views.gestion_index, name='gestion'),
+    # path('', views.gestion_index, name='gestion_index'),
 
     path('nuevo/', views.gestion_nuevo, name='gestion_nuevo'),
     path('buscar/', views.gestion_buscar, name='gestion_buscar'),
@@ -12,8 +12,9 @@ urlpatterns = [
     path('eliminar/<int:id>', views.gestion_eliminar, name='gestion_eliminar'),
     
     # -- CBV --
+    path('', views.GestionListView.as_view(), name='gestion'),
     path('cbvlista/', views.GestionListView.as_view(), name='gestioncbv_lista'),
-    path('cbvnuevo/<int:pk>/', views.GestionCreateView.as_view(), name='gestioncbv_nuevo'),
+    path('cbvnuevo/', views.GestionCreateView.as_view(), name='gestioncbv_nuevo'),
     path('cbvdetalle/<int:pk>/', views.GestionDetailView.as_view(), name='gestioncbv_detalle'),
     path('cbveditar/<int:pk>/', views.GestionUpdateView.as_view(), name='gestioncbv_editar'),
     path('cbvborrar/<int:pk>/', views.GestionDeleteView.as_view(), name='gestiocbv_borrar'),
