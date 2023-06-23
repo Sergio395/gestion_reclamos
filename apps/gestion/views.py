8ba2f656575ee460c260ce11209216df0a150d96
Ezequiel Mazzini <ezequiel.mazzini@bue.edu.ar>
2023-06-23 02:50:27 -0300

    filtros funcionando

--- apps/gestion/views.py
+++ apps/gestion/views.py
@@ -144,65 +144,12 @@ def preparar_filtro(criterio):
 
 @method_decorator([login_required, group_required(*AUTORIZED_GROUPS)], name='dispatch')
 class GestionListView(ListView):
-    '''
-    Muestra una lista de inspecciones
-    '''
-    model = GestionModel
-    queryset = GestionModel.objects.filter(eliminado=False)
-    paginate_by = 10
-    template_name = 'gestion/gestioncbv_lista.html'
-    ordering = ['id']
-
-    def get_context_data(self, **kwargs):
-        '''
-        Agrega el formulario de busqueda al contexto
-        '''
-        context = super(GestionListView, self).get_context_data(**kwargs)
-        context['form_busqueda'] = BusquedaGestionForm()
-        return context
-
-    def post(self, request, *args, **kwargs):
-        """Procesa el formulario enviado por POST.
-
-        Si el formulario de busqueda es validos, se filtran las inspecciones y se devuelve a la lista.
-        """
-        busqueda_form = BusquedaForm(request.POST)
-        if busqueda_form.is_valid():
-            return self.form_valid(busqueda_form)
-        else:
-            return self.form_invalid(busqueda_form)
-        
-# ------------ voy x aca -----------
-
-    def form_valid(self, busqueda_form):
-        """Guarda los datos actualizados del reclamo y denunciante.
-
-        Redirige a la página de seguimiento después de guardar los cambios y muestra un mensaje de éxito.
-        """
-        messages.success(self.request, 'Resultados de la busqueda')
-        filtro = preparar_filtro(busqueda_form.cleaned_data)
-        gestion = GestionModel.objects.filter(**filtro)
-        self.success_url = reverse_lazy('gestioncbv_lista')
-        return redirect(self.success_url)
-
-    def form_invalid(self, busqueda_form):
-        """Maneja el caso cuando el formulario es inválido.
-
-        Muestra un mensaje de error y renderiza nuevamente el formulario con los datos ingresados.
-        """
-        messages.error(self.request, 'Revisa los campos del formulario')
-        return self.render_to_response(self.get_context_data(busqueda_form)) 
-
-#------------Robandole a Cristian (Gracias!!)-----
-
-class GestionListView2(ListView):
     """Vista para mostrar una lista de Gestion.
 
     Muestra una lista de gestion que no han sido eliminados.
     """
     model = GestionModel
     template_name = 'gestion/gestioncbv_lista.html'
-    # context_object_name = 'relaciones'
     queryset = GestionModel.objects.filter(eliminado=False)
     paginate_by = 10
     ordering = ['id']
@@ -238,25 +185,13 @@ class GestionListView2(ListView):
     def get_context_data(self, **kwargs):
         """Obtiene los datos del contexto para la vista.
 
-        Retorna el contexto actualizado con las relaciones reclamo-denunciante.
+        Retorna el contexto actualizado
         """
         context = super().get_context_data(**kwargs)
         context['form_busqueda'] = BusquedaGestionForm(self.request.GET or None)
         
         return context
 
-    # def get(self, request, *args, **kwargs):
-    #     """Maneja la solicitud GET para la vista.
-
-    #     Si se proporciona el parámetro 'reset_filters' en la URL, redirecciona a la URL de la vista sin los parámetros de filtro.
-    #     De lo contrario, retorna la página de creación de reclamo con los campos de formulario vacíos.
-    #     """
-    #     # if 'reset_filters' in request.GET:
-    #     #     # Redireccionar a la URL de la vista sin los parámetros de filtro
-    #     #     return HttpResponseRedirect(reverse('gestion'))
-    #     return super().get(request, *args, **kwargs)
-    
-#---------------- Fin de Robo --------
 
 @method_decorator([login_required, group_required(*AUTORIZED_GROUPS)], name='dispatch')
 class InspeccionListView(ListView):
@@ -269,37 +204,43 @@ class InspeccionListView(ListView):
     queryset = inspecciones.objects.filter(eliminado=False)
     ordering = ['id']
     
+    def get_queryset(self):
+        """Obtiene el conjunto de consultas (queryset) para la vista.
+
+        Recupera los datos del filtro pasados por contexto.
+        """
+        queryset = super().get_queryset()
+        c1_c = self.request.GET.get('criterio1_campo', 'none')
+        c1_v = self.request.GET.get('criterio1_valor', 'none')
+        c2_c = self.request.GET.get('criterio2_campo', 'none')
+        c2_v = self.request.GET.get('criterio2_valor', 'none')
+        c3_c = self.request.GET.get('criterio3_campo', 'none')
+        c3_v = self.request.GET.get('criterio3_valor', 'none')
+        c4_c = self.request.GET.get('criterio4_campo', 'none')
+        c4_v = self.request.GET.get('criterio4_valor', 'none')
+
+        # verifico si existen y arma el diccionario de fitro
+        filtro = {'eliminado':False}
+    
+        if c1_c != 'none':
+            filtro[c1_c] = c1_v
+        if c2_c != 'none':
+            filtro[c2_c] = c2_v
+        if c3_c != 'none':
+            filtro[c3_c] = c3_v
+        if c4_c != 'none':
+            filtro[c4_c] = c4_v.strftime('%Y-%m-%d')
+        return queryset.filter(**filtro)
+
     def get_context_data(self, **kwargs):
-        '''
-        Agrega el formulario de busqueda al contexto
-        '''
-        context = super(InspeccionListView, self).get_context_data(**kwargs)
-        context['form_busqueda'] = BusquedaForm()
-        return context
+        """Obtiene los datos del contexto para la vista.
 
-    def post(self, request, *args, **kwargs):
-        """Procesa el formulario enviado por POST.
-
-        Si el formulario de busqueda es valido, se filtran las inspecciones y se devuelve a la lista.
+        Retorna el contexto actualizado y el formulario.
         """
-        busqueda_form = BusquedaForm(request.POST)
-        if busqueda_form.is_valid():
-            return self.form_valid(busqueda_form)
-        else:
-            return self.form_invalid(busqueda_form)
+        context = super().get_context_data(**kwargs)
+        context['form_busqueda'] = BusquedaForm(self.request.GET or None)
         
-# ------------ voy x aca -----------
-
-    def form_valid(self, busqueda_form):
-        """Guarda los datos actualizados del reclamo y denunciante.
-
-        Redirige a la página de seguimiento después de guardar los cambios y muestra un mensaje de éxito.
-        """
-        messages.success(self.request, 'Resultados de la busqueda')
-        filtro = preparar_filtro(busqueda_form.cleaned_data)
-        inspeccion = inspecciones.objects.filter(**filtro)
-        self.success_url = reverse_lazy('inspeccioncbv_lista')
-        return redirect(self.success_url, inspecciones=inspeccion)
+        return context
 
 @method_decorator([login_required, group_required(*AUTORIZED_GROUPS)], name='dispatch')
 class GestionDetailView(DetailView):
